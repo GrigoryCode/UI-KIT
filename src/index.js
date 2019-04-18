a6429a49c93a34fd28233e3eddac946e08af674e
GrigoryCode <grigorycode@gmail.com>
2019-04-18 14:46:52 +0300

    Add nav-bar

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import '../src/index.scss';
-import 'normalize.css';
-import jQuery from 'jquery';
-
-console.log('Hello index.js!')
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import '../src/main.scss';
+import 'normalize.css';
+import jQuery from 'jquery';
+
+console.log('Hello main.js!')
